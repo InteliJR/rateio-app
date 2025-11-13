33f2cecd9d4c2b8385d8f5487ca08f7b8714ce2b
MarcusML <114195461+m4rcusml@users.noreply.github.com>
2025-11-12 21:14:27 -0300

    feat: implement tabbar and complete navigation

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -1,11 +1,22 @@
-// mobile/app/(tabs)/_layout.tsx
+import { Tabs } from 'expo-router';
 
-import { Stack } from 'expo-router';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 
 export default function TabsLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" />
-    </Stack>
+    <Tabs screenOptions={{ headerShown: false, tabBarActiveTintColor: '#000', tabBarInactiveTintColor: '#0009' }}>
+      <Tabs.Screen name="scanner" options={{
+        tabBarIcon: ({ color }) => <MaterialCommunityIcons name="camera-outline" size={24} color={color} />,
+        tabBarLabel: 'Scanner'
+      }} />
+      <Tabs.Screen name="accounts" options={{
+        tabBarIcon: ({ color }) => <MaterialCommunityIcons name="invoice-text-clock-outline" size={24} color={color} />,
+        tabBarLabel: 'Contas'
+      }} />
+      <Tabs.Screen name="profile" options={{
+        tabBarIcon: ({ color }) => <MaterialCommunityIcons name="account-outline" size={24} color={color} />,
+        tabBarLabel: 'Perfil'
+      }} />
+    </Tabs>
   );
 }
